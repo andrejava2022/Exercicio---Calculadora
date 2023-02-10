1e70c8e52beb66a1b597fedcd27dc78981cf6e70
André Luiz da Silva <111994982+andrejava2022@users.noreply.github.com>
2023-02-10 18:56:42 -0300

    Exercicio realizado durante curso
    Estou aprendendo a utilizar o Git e GitHub, irei postar as atividades que ja fiz e faço.

---
+++ Calculado.java
@@ -0,0 +1,42 @@
+public class Calculadora{
+	
+	public static void main(String args[]){
+		
+		double num1 = Double.parseDouble(args[0]); 
+		double num2 = Double.parseDouble(args[2]); 
+		
+		String operacao = args[1]; 
+		
+		double resultado = 0; 
+		
+		switch(operacao){
+			default: 
+			    System.out.println("Operação inválida!");
+			break; 
+			case "+": 
+			   resultado = num1 + num2; 
+			   System.out.println("O resultado da soma é: " + resultado); 
+			   break; 
+			case "-": resultado = num1 - num2; 
+			   System.out.println("O resultado da subtração é: " + resultado); 
+			   break; 
+            case "X":			
+			case "x": resultado = num1 * num2; 
+			   System.out.println("O resultado da multiplicação é: " + resultado); 
+			   break;  
+			case "/": 
+			case ":":  
+			
+			if(num2 == 0){
+				System.out.println("Não é possível dividir por 0.");
+			}else{ 
+			     resultado = num1 / num2; 
+			   System.out.println("O resultado da divisão é: " + resultado); 
+			}
+			   break;
+			 
+		}
+		
+	}
+	
+}
